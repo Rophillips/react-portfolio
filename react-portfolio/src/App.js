a350a39392f974a6f49a95242433fef7c1fc7120
Roseanne Phillips <rphillips.rp@gmail.com>
2021-05-09 17:02:38 -0400

    trying to add projects

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component }from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/pages/Home";
@@ -9,31 +9,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
 import Projects from './Projects.json';
 import ProjectCard from "./components/ProjectCard";
+import Wrapper from "./components/Wrapper";
 
 
 class App extends Component {
   state = {
     Projects: [...Projects]
   }
-
+  handleRemove = id => {
+    // console.log(this.state);
+    this.setState({
+      Projects: this.state.Projects.filter(Project => Project.id !== id)
+    });
+  }
 
 render() {
   return (
     <Router>
       <div>
         <Header/>
+        <Wrapper/>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/Project" component={Project} />
-        <Route path="/ProjectCard" component={ProjectCard} />
+        <Route exact path="/project" component={Project} />
+        <Route path="/projectCard" component={ProjectCard} />
         <Route path="/contact" component={Contact} />
-        {this.state.project.map( project => (
+        <Wrapper/>
+        {/* {this.state.project.map( project => (
           <ProjectCard
             key={project.id}
             handleRemove={this.handleRemove}
             {...project}
           />
-        ))}
+        ))} */}
         
         </div>
         <div className="/Footer">
--- react-portfolio/src/components/ProjectCard/index.js
+++ react-portfolio/src/components/ProjectCard/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./index.js";
 import "./style.css";
 //import "./components/pages/Project";
